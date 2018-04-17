5aebe8d614e24e961f4ecc1275b420fd2e633776
Aaron Kaufer <electricaaron.com@gmail.com>
2018-04-17 13:47:56 -0700

    Initial Commit

---
+++ BFAIClean/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  BFAIClean
+//
+//  Created by Aaron Kaufer on 4/17/18.
+//  Copyright © 2018 Aaron Kaufer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
